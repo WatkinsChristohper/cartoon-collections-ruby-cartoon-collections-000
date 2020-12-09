4ff9b67341a35c4ab31ab66c17ba57d12eefbbba
Christopher Watkins <chriswatkins7@yahoo.com>
2020-12-09 02:24:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,12 @@
-dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy", "Happy","Sleepy","Sneezy"]
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-call_screams = ["bo", "bam", "boom", "bazam", "powzer"]
 
-def roll_call_dwarves(array)
-  i = 0 
-  while i < array.length 
-    puts "#{i + 1}. #{array[i]}"
-    i += 1 
-  end 
+def roll_call_dwarves(dwarves)
+  counter = 1
+  dwarves.each do |names|
+  puts "#{counter}. #{names}"
+  counter += 1
+  end
 end
 
 roll_call_dwarves(dwarves)
@@ -23,23 +22,27 @@ def summon_captain_planet(array)
   nu_array
 end
 
+
 summon_captain_planet(planeteer_calls)
 
-def long_planeteer_calls(array) 
-  i = 0 
-  if  array.any? {|i| i.length > 4}
+
+def long_planeteer_calls(planeteer_calls)
+   planeteer_calls.each do |calls|
+   if calls.length > 4
     return true
-  else 
+   else 
     return false
-  i = i + 1
-  end 
+   end
+  end
+
 end
 
-long_planeteer_calls(call_screams)
+long_planeteer_calls(planeteer_calls)
+
+
 
 cheese_types = ["cheddar", "gouda", "camembert"]
 snacks = ["crackers", "gouda", "thyme"]
-
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
